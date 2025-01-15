b2bebd2d5992440963c0b2c6cf4564e5acec0f70
Michael Himmelberg <michael.himmelberg@gmx.de>
2025-01-15 13:52:09 +0100

    default -> SparseContextFlowSensitive

--- soot-infoflow/src/soot/jimple/infoflow/InfoflowConfiguration.java
+++ soot-infoflow/src/soot/jimple/infoflow/InfoflowConfiguration.java
@@ -997,7 +997,7 @@ public class InfoflowConfiguration {
 	 *
 	 */
 	public static class SolverConfiguration {
-		private DataFlowSolver dataFlowSolver = DataFlowSolver.ContextFlowSensitive;
+		private DataFlowSolver dataFlowSolver = DataFlowSolver.SparseContextFlowSensitive;
 		private SparsePropagationStrategy sparsePropagationStrategy = SparsePropagationStrategy.Precise;
 		private int maxJoinPointAbstractions = 10;
 		private int maxCalleesPerCallSite = 75;
