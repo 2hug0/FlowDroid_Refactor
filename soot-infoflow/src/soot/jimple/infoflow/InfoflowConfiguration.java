ed06898c01118fe839e7e04092866fd676a4d777
Michael Himmelberg <michael.himmelberg@gmx.de>
2025-02-15 14:42:21 +0100

    Sparsesolver test

--- soot-infoflow/src/soot/jimple/infoflow/InfoflowConfiguration.java
+++ soot-infoflow/src/soot/jimple/infoflow/InfoflowConfiguration.java
@@ -1002,7 +1002,7 @@ public class InfoflowConfiguration {
 	 *
 	 */
 	public static class SolverConfiguration {
-		private DataFlowSolver dataFlowSolver = DataFlowSolver.MergeContextFlowSensitive;
+		private DataFlowSolver dataFlowSolver = DataFlowSolver.SparseContextFlowSensitive;
 		private SparsePropagationStrategy sparsePropagationStrategy = SparsePropagationStrategy.Precise;
 		private int maxJoinPointAbstractions = 10;
 		private int maxCalleesPerCallSite = 75;
